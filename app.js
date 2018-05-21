361ebdae3d87c7d4718649e00ca838da7b7d3dcb
vanthe14 <vanthe14@gmail.com>
2018-05-21 16:52:22 +0700

    create flags folder

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var path    = require('path');
 var http = require('http');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var i18n = require('i18n')
 var mongoose = require("mongoose");
+
 // in the requirements section
 var ejsLayouts = require("express-ejs-layouts");
 
@@ -30,6 +32,38 @@ app.set("view engine", "ejs");
 app.set('layout', path.join(__dirname,'/templates/layouts/home_layout'));
 app.use(ejsLayouts);
 app.set("views", path.join(__dirname, 'templates/views')); 
+
+app.use(cookieParser());
+// setting i18n
+i18n.configure({
+    locales: ['en', 'vi'],
+    register: global,
+    fallbacks: {'en' : 'vi'},
+    cookie: 'language',
+    queryParameter: 'lang',
+    defaultLocale: 'en',
+    directory: __dirname + '/languages',
+    autoReload: true,
+    updateFiles: true,
+    api: {
+        '__' : '__',
+        '__n' : '__n'
+    },
+    
+})
+app.use(function(req, res, next){
+    i18n.init(req, res, next);
+});
+app.use(function(req, res, next){
+    res.locals.clanguage = req.getLocale();
+    res.locals.languages = i18n.getLocales();
+    next();
+})
+app.use("/change-lang/:lang", (req, res) => { 
+    res.cookie('language', req.params.lang, { maxAge: 900000 });
+    res.redirect('back');
+   // console.log("hear")
+});
 
 // Use Router Frontend
 app.use('/', homepage_Router);
